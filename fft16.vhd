0cace65e2bebcb056a4f81f30963ef2a430a8bf3
a-ebrahimi-d <81751416+a-ebrahimi-d@users.noreply.github.com>
2021-12-10 11:56:39 +0330

    Update fft16.vhd

--- fft16.vhd
+++ fft16.vhd
@@ -57,6 +57,9 @@ signal int_sig14 : signed (11 downto 0) := (others => '0');
 signal int_sig15 : signed (11 downto 0) := (others => '0');
 signal int_sig16 : signed (11 downto 0) := (others => '0');
 
+signal intersig2 : signed (19 downto 0) := (others => '0');
+
+
 component fft4_1 is
     Port ( Clk   : in STD_LOGIC;
 			  in1   : in   signed(7 downto 0);
@@ -106,16 +109,41 @@ component fft4_4 is
 end component;
 
 begin
-	m_1: fft4_1 port map(in1 => in1, in2 => in5, in3 => in9, in4 => in13 ,Clk => Clk, out1 => int_sig1,  out2 => int_sig2,  out3 => int_sig3, out4 => int_sig4);
-	m_2: fft4_2 port map(in1 => in1, in2 => in5, in3 => in9, in4 => in13 ,Clk => Clk, out1 => int_sig5,  out2 => int_sig6,  out3 => int_sig7, out4 => int_sig8);
-	m_3: fft4_3 port map(in1 => in1, in2 => in5, in3 => in9, in4 => in13 ,Clk => Clk, out1 => int_sig9,  out2 => int_sig10, out3 => int_sig11, out4 => int_sig12);
-	m_4: fft4_4 port map(in1 => in1, in2 => in5, in3 => in9, in4 => in13 ,Clk => Clk, out1 => int_sig13, out2 => int_sig14, out3 => int_sig15, out4 => int_sig16);
+	m_1: fft4_1 port map(in1 => in1, in2 => in5, in3 => in9,  in4 => in13 ,Clk => Clk, out1 => int_sig1,  out2 => int_sig2,  out3 => int_sig3, out4 => int_sig4);
+	m_2: fft4_2 port map(in1 => in2, in2 => in6, in3 => in10, in4 => in14 ,Clk => Clk, out1 => int_sig5,  out2 => int_sig6,  out3 => int_sig7, out4 => int_sig8);
+	m_3: fft4_3 port map(in1 => in3, in2 => in7, in3 => in11, in4 => in15 ,Clk => Clk, out1 => int_sig9,  out2 => int_sig10, out3 => int_sig11, out4 => int_sig12);
+	m_4: fft4_4 port map(in1 => in4, in2 => in8, in3 => in12, in4 => in16 ,Clk => Clk, out1 => int_sig13, out2 => int_sig14, out3 => int_sig15, out4 => int_sig16);
 
 process(Clk)
 		begin
 			if (rising_edge(Clk)) then
 					
-					
+					out1  <= int_sig1 + int_sig5 + int_sig9 + int_sig13;
+					out5(5 downto 0)  <= int_sig1(5 downto 0)  - int_sig5(11 downto 6) - int_sig9(5 downto 0)  + int_sig13(11 downto 6);
+					out5(11 downto 6) <= int_sig1(11 downto 6) + int_sig5(5 downto 0)  - int_sig9(11 downto 6) - int_sig13(5 downto 0);
+					out9  <= int_sig1 - int_sig5 + int_sig9 - int_sig13;
+					out13(5 downto 0)  <= int_sig1(5 downto 0)  + int_sig5(11 downto 6) - int_sig9(5 downto 0)  - int_sig13(11 downto 6);
+					out13(11 downto 6) <= int_sig1(11 downto 6) - int_sig5(5 downto 0)  - int_sig9(11 downto 6) + int_sig13(5 downto 0);
+					intersig2(9 downto 0)   <= int_sig2(5 downto 0)  + "1110" * int_sig6(5 downto 0)  + "1011" * int_sig10(5 downto 0)  + "0110" * int_sig14(5 downto 0) - "0110" * int_sig6(11 downto 6)  - "1011" * int_sig10(11 downto 6)  - "1110" * int_sig14(11 downto 6);
+					intersig2(19 downto 10) <= int_sig2(11 downto 6) + "1110" * int_sig6(11 downto 6) + "1011" * int_sig10(11 downto 6) + "0110" * int_sig14(11 downto 6)+ "0110" * int_sig6(5 downto 0)   + "1011" * int_sig10(5 downto 0)   + "1110" * int_sig14(5 downto 0);
+					intersig6(9 downto 0)   <= int_sig2(5 downto 0)  - "0110" * int_sig6(5 downto 0)  - "1011" * int_sig10(5 downto 0)  + "1110" * int_sig14(5 downto 0) - "1110" * int_sig6(11 downto 6)  + "1011" * int_sig10(11 downto 6)  + "0110" * int_sig14(11 downto 6);
+					intersig6(19 downto 10) <= int_sig2(11 downto 6) - "0110" * int_sig6(11 downto 6) - "1011" * int_sig10(11 downto 6) + "1110" * int_sig14(11 downto 6)+ "1110" * int_sig6(5 downto 0)   - "1011" * int_sig10(5 downto 0)   - "0110" * int_sig14(5 downto 0);
+					intersig10(9 downto 0)   <= int_sig2(5 downto 0)  - "1110" * int_sig6(5 downto 0)  + "1011" * int_sig10(5 downto 0)  - "0110" * int_sig14(5 downto 0) + "0110" * int_sig6(11 downto 6)  - "1011" * int_sig10(11 downto 6)  + "1110" * int_sig14(11 downto 6);
+					intersig10(19 downto 10) <= int_sig2(11 downto 6) - "1110" * int_sig6(11 downto 6) + "1011" * int_sig10(11 downto 6) - "0110" * int_sig14(11 downto 6)- "0110" * int_sig6(5 downto 0)   + "1011" * int_sig10(5 downto 0)   - "1110" * int_sig14(5 downto 0);
+					intersig14(9 downto 0)   <= int_sig2(5 downto 0)  - "1110" * int_sig6(5 downto 0)  + "1011" * int_sig10(5 downto 0)  - "0110" * int_sig14(5 downto 0) + "0110" * int_sig6(11 downto 6)  - "1011" * int_sig10(11 downto 6)  + "1110" * int_sig14(11 downto 6);
+					intersig14(19 downto 10) <= int_sig2(11 downto 6) - "1110" * int_sig6(11 downto 6) + "1011" * int_sig10(11 downto 6) - "0110" * int_sig14(11 downto 6)- "0110" * int_sig6(5 downto 0)   + "1011" * int_sig10(5 downto 0)   - "1110" * int_sig14(5 downto 0);
+						
+					out6  <= int_sig2 + int_sig6 + int_sig10 + int_sig14;
+					out10 <= int_sig2 + int_sig6 + int_sig10 + int_sig14;
+					out14 <= int_sig2 + int_sig6 + int_sig10 + int_sig14;
+					out3  <= int_sig3 + int_sig7 + int_sig11 + int_sig15;
+					out7  <= int_sig3 + int_sig7 + int_sig11 + int_sig15;
+					out11 <= int_sig3 + int_sig7 + int_sig11 + int_sig15;
+					out15 <= int_sig3 + int_sig7 + int_sig11 + int_sig15;
+					out4  <= int_sig4 + int_sig8 + int_sig12 + int_sig16;
+					out8  <= int_sig4 + int_sig8 + int_sig12 + int_sig16;
+					out12 <= int_sig4 + int_sig8 + int_sig12 + int_sig16;
+					out16 <= int_sig4 + int_sig8 + int_sig12 + int_sig16;
 					
 			end if;
 end process;
